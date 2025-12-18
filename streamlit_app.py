75448c54d2137d6fc5bce8705f1ef4a4facee215
CWY <cranageist@gmail.com>
2025-12-18 15:38:46 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ env = environment.WaterHeaterEnv()
 agent = PPOAgent(4, (6,))
 obs, _ = env.reset()
 total_reward = 0.0
+print("a")
 
 st.header("Water Heater RL Approach")
 option = st.selectbox("Pick model to use", ("PPO", "SAC", "A2C", "DQN"), index = 0, placeholder = "Select model...")
@@ -57,7 +58,6 @@ if st.button("Reset", type = "primary"):
     st.divider()
     st.write(f"Room Temperature: {env.ROOM_TEMP:.2f}C")
     st.write(f"Water Temperature: {env.water_tank_temp:.2f}C")
-    st.write(f"Temperature Loss: {env.temp_loss:.2f}C")
 
 if st.button("Step", disabled = st.session_state.get("disabled", True)):
     action = agent.get_action(obs)
@@ -71,7 +71,6 @@ if st.button("Step", disabled = st.session_state.get("disabled", True)):
     st.divider()
     st.write(f"Room Temperature: {env.ROOM_TEMP:.2f}C")
     st.write(f"Water Temperature: {env.water_tank_temp:.2f}C")
-    st.write(f"Temperature Loss: {env.temp_loss:.2f}C")
     st.divider()
     st.write(f"Comfort Reward: {info["rewards"]["comfort"]:.2f}C")
     st.write(f"Hygiene Reward: {info["rewards"]["hygiene"]:.2f}C")
