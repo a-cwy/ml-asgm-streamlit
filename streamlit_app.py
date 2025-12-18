9e0f641b89317558d4d76a02442b0fb11df583d3
CWY <cranageist@gmail.com>
2025-12-18 15:01:50 +0800

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import environment
+import gymnasium as gym
+import numpy as np
+from ppo import PPOAgent
+from sac import SACAgent
+from dqn import DQNAgent
+from a2c import A2CAgent
 
-st.write("Hello **world**!")
+environment.init()
+env = gym.make('WaterHeater-v0')
+
+st.header("Water Heater RL Approach")
+st.divider()
+st.write(env.ROOM_TEMP)
