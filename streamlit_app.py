2c1e4a98824d63d49504d810bae4171c4426ef29
CWY <cranageist@gmail.com>
2025-12-18 15:48:54 +0800

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,10 @@ from a2c import A2CAgent
 
 st.header("Water Heater RL Approach")
 option = st.selectbox("Pick model to use", ("PPO", "SAC", "A2C", "DQN"), index = 0, placeholder = "Select model...")
-if st.button("Select"):
+if st.button("Run"):
     env = environment.WaterHeaterEnv()
     obs, _ = env.reset()
     total_reward = 0.0
-    st.session_state.disabled = False
 
     match option:
         case "PPO":
@@ -32,47 +31,24 @@ if st.button("Select"):
             agent = A2CAgent(env)
             pass
 
-if st.button("Reset", type = "primary"):
-    obs, _ = env.reset()
-    total_reward = 0.0
-    st.session_state.disabled = False
+    done = False
 
-    match option:
-        case "PPO":
-            agent = PPOAgent(4, (6,))
-            pass
-        case "SAC":
-            agent = SACAgent()
-            pass
-        case "A2C":
-            agent = DQNAgent(env, 6, 4)
-            agent.load_models('dash/target_dqn.keras')
-            pass
-        case "DQN":
-            agent = A2CAgent(env)
-            pass
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, term, trun, info = env.step(action)
+        done = term or trun
+        total_reward = total_reward + reward
+        obs = next_obs
 
-    st.divider()
-    st.write(f"Room Temperature: {env.ROOM_TEMP:.2f}C")
-    st.write(f"Water Temperature: {env.water_tank_temp:.2f}C")
-    st.write(f"Temperature Change: {env.temp_loss:.2f}C")
+        st.divider()
+        st.write(f"Room Temperature: {env.ROOM_TEMP:.2f}C")
+        st.write(f"Water Temperature: {env.water_tank_temp:.2f}C")
+        st.write(f"Temperature Change: {env.temp_loss:.2f}C")
+        st.divider()
+        st.write(f"Comfort Reward: {info["rewards"]["comfort"]:.2f}C")
+        st.write(f"Hygiene Reward: {info["rewards"]["hygiene"]:.2f}C")
+        st.write(f"Energy Reward: {info["rewards"]["energy"]:.2f}C")
+        st.write(f"Safety Reward: {info["rewards"]["safety"]:.2f}C")
+        st.write(f"Step Reward: {reward:.2f}C")
 
-if st.button("Step", disabled = st.session_state.get("disabled", True)):
-    action = agent.get_action(obs)
-    next_obs, reward, term, trun, info = env.step(action)
-    total_reward = total_reward + reward
-    obs = next_obs
-
-    if (term or trun):
-        st.session_state.disabled = True
-
-    st.divider()
-    st.write(f"Room Temperature: {env.ROOM_TEMP:.2f}C")
-    st.write(f"Water Temperature: {env.water_tank_temp:.2f}C")
-    st.write(f"Temperature Change: {env.temp_loss:.2f}C")
-    st.divider()
-    st.write(f"Comfort Reward: {info["rewards"]["comfort"]:.2f}C")
-    st.write(f"Hygiene Reward: {info["rewards"]["hygiene"]:.2f}C")
-    st.write(f"Energy Reward: {info["rewards"]["energy"]:.2f}C")
-    st.write(f"Safety Reward: {info["rewards"]["safety"]:.2f}C")
-    st.write(f"Step Reward: {reward:.2f}C")
+    st.write(f"Total Reward for Episode: {total_reward:.2f}")
